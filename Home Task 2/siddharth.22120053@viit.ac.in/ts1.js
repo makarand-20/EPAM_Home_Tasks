edebd7a3e127100287e062ad0f26c9230dfcdc63
Makarand Khiste <111168325+makarand-20@users.noreply.github.com>
2023-04-16 23:31:56 +0530

    Revert "task2-sid"

--- Home Task 2/siddharth.22120053@viit.ac.in/ts1.js
+++
@@ -1,9 +0,0 @@
-// Problem statement Link
-//http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-
-
-// Solution
-function printArray(array){
-    return array.join(',');
-  }
-
--- Home Task 2/siddharth.22120053@viit.ac.in/ts2.js
+++
@@ -1,9 +0,0 @@
-
-// Problem statement Link
-//http://www.codewars.com/kata/opposite-number
-
-
-// Solution
-function opposite(number) {
-    return(-number);
-}
