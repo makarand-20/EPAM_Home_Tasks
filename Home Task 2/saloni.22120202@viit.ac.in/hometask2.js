10a4ccc881c79202c0257a5d3614b0ab3059c8ea
Saloni <Saloniwagh2@gmail.com>
2023-04-16 23:43:42 +0530

    commit2

--- Home Task 2/saloni.22120202@viit.ac.in/hometask2.js
+++ Home Task 2/saloni.22120202@viit.ac.in/hometask2.js
@@ -31,3 +31,20 @@ function basicOp(operation, value1, value2)
   }
 }
 
+function basicOp(operation, value1, value2)
+{
+  switch(operation){
+      case '+':
+        return value1 + value2;
+      case '-':
+        return value1 - value2;    
+      case '*':
+        return value1 * value2;
+      case '/':
+        return value1 / value2;
+      default:
+        return 0;
+      
+  }
+}
+
