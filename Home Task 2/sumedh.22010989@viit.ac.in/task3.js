888b96b4b70b9a984ad9125897bf06970961db21
Sumedh Kulkarni <126947403+sumedhk230@users.noreply.github.com>
2023-04-17 11:38:11 +0530

    Add files via upload

---
+++ Home Task 2/sumedh.22010989@viit.ac.in/task3.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/basic-mathematical-operations
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
