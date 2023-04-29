1b338c4f77ae2319e89cba1a39b80b365f109b6a
Sanika1363 <122290849+Sanika1363@users.noreply.github.com>
2023-04-29 12:17:30 +0530

    Add files via upload

---
+++ Home Task 2/sanika.22010844@viit.ac.in/task3.js
@@ -0,0 +1,16 @@
+//Basic Mathematical Operations
+
+function basicOperations(operator,value1,value2){
+    switch(operator){
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 /value2;
+        default:
+            return 0;
+    }
+}
